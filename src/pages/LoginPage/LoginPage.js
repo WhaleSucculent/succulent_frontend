f27378d52c4a9275bcbf7d67a35d081c68c5cf16
SHENG MING YAN <ysmsam@hotmail.com>
2022-06-15 14:06:47 -0600

    commit

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,9 +1,188 @@
-import React from 'react'
+// import * as React from 'react';
+// import Avatar from '@mui/material/Avatar';
+// import Button from '@mui/material/Button';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+// import Link from '@mui/material/Link';
+// import Grid from '@mui/material/Grid';
+// import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import Typography from '@mui/material/Typography';
+// import Container from '@mui/material/Container';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// function Copyright(props) {
+//   return (
+//     <Typography variant="body2" color="text.secondary" align="center" {...props}>
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
+
+// const theme = createTheme();
+
+// export default function SignIn() {
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.currentTarget);
+//     console.log({
+//       email: data.get('email'),
+//       password: data.get('password'),
+//     });
+//   };
+
+//   return (
+//     <ThemeProvider theme={theme}>
+//       <Container component="main" maxWidth="xs">
+//         <CssBaseline />
+//         <Box
+//           sx={{
+//             marginTop: 8,
+//             display: 'flex',
+//             flexDirection: 'column',
+//             alignItems: 'center',
+//           }}
+//         >
+//           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+//             <LockOutlinedIcon />
+//           </Avatar>
+//           <Typography component="h1" variant="h5">
+//             Sign in
+//           </Typography>
+//           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+//             <TextField
+//               margin="normal"
+//               required
+//               fullWidth
+//               id="email"
+//               label="Email Address"
+//               name="email"
+//               autoComplete="email"
+//               autoFocus
+//             />
+//             <TextField
+//               margin="normal"
+//               required
+//               fullWidth
+//               name="password"
+//               label="Password"
+//               type="password"
+//               id="password"
+//               autoComplete="current-password"
+//             />
+//             <FormControlLabel
+//               control={<Checkbox value="remember" color="primary" />}
+//               label="Remember me"
+//             />
+//             <Button
+//               type="submit"
+//               fullWidth
+//               variant="contained"
+//               sx={{ mt: 3, mb: 2 }}
+//             >
+//               Sign In
+//             </Button>
+//             <Grid container>
+//               <Grid item xs>
+//                 <Link href="#" variant="body2">
+//                   Forgot password?
+//                 </Link>
+//               </Grid>
+//               <Grid item>
+//                 <Link href="#" variant="body2">
+//                   {"Don't have an account? Sign Up"}
+//                 </Link>
+//               </Grid>
+//             </Grid>
+//           </Box>
+//         </Box>
+//         <Copyright sx={{ mt: 8, mb: 4 }} />
+//       </Container>
+//     </ThemeProvider>
+//   );
+// }
+
+// -------------------------------------------------------------------
+import React from "react";
+import "css/style.scss";
+import "css/app.scss";
+
+import Header from "./Header";
+
+const state = {
+  email: "",
+  password: "",
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  console.log(this.state);
+  this.props.history.push("/");
+};
+
+const handleChange = (e) => {
+  this.setState({
+    // email: e.target.value.toUpperCase()
+    [e.target.name]: e.target.value,
+  });
+};
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <React.Fragment>
+      <Header />
+      <div className="login-wrapper">
+        <form className="box login-box" onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label className="label">Email</label>
+            <p className="control has-icons-left has-icons-right">
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+              <input
+                className="input"
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+              <span className="icon is-small is-right">
+                <i className="fas fa-check"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <label className="label">Password</label>
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+            </p>
+          </div>
 
-export default LoginPage
+          <div className="control">
+            <button className="button is-fullwidth is-link">Login</button>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default LoginPage;
--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+import "css/style.scss";
+import "css/app.scss";
+
+import Header from "./Header";
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
-  )
-}
+    <React.Fragment>
+      <Header />
+      <div className="login-wrapper">
+        <form className="box login-box" onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label className="label">Email</label>
+            <p className="control has-icons-left has-icons-right">
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+              <input
+                className="input"
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+              <span className="icon is-small is-right">
+                <i className="fas fa-check"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <label className="label">Password</label>
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-check"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <label className="label">Re-Password</label>
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="password"
+                placeholder="Re-Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+              <span className="icon is-small is-left">
+                <i className="fas fa-lock"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-check"></i>
+              </span>
+            </p>
+          </div>
 
-export default RegisterPage
+          <div className="control">
+            <button className="button is-fullwidth is-link">Regrist</button>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default RegisterPage;
