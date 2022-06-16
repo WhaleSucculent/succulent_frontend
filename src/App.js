7c7b23f1731afbb64c8d92b777fc3f6fb1905592
Clockoranges <jiasheng.luu@gmail.com>
2022-06-16 12:53:27 -0600

    chore: carousel update

--- src/App.js
+++ src/App.js
@@ -22,42 +22,70 @@ import CollectionsPage from "pages/CollectionsPage/CollectionsPage";
 import Nav from "components/Nav";
 import Cart from "pages/CheckoutPage/Cart";
 
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: "https://succulentbackend.azurewebsites.net/graphql",
+  cache,
+});
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          {/* Router for landing page */}
-          <Route path="landing" element={<LandingPage />} />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Routes>
+            {/* Router for landing page */}
+            <Route path="landing" element={<LandingPage />} />
 
-          {/* Router for   */}
-          <Route path="/" element={<Nav />}>
-            <Route index element={<HomePage />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="register" element={<RegisterPage />} />
-            <Route path="products" element={<ProductsPage />}>
-              <Route path=":productsId" element={<ProductDetailPage />} />
+            {/* Router for   */}
+            <Route path="/" element={<Nav />}>
+              <Route index element={<HomePage />} />
+              <Route path="login" element={<LoginPage />} />
+              <Route path="register" element={<RegisterPage />} />
+              <Route path="products" element={<ProductsPage />}>
+                <Route path=":productsId" element={<ProductDetailPage />} />
+              </Route>
+              <Route path="collections" element={<CollectionsPage />}>
+                <Route path="new" element={<CollectionsPage />} />
+                <Route path="rare" element={<CollectionsPage />} />
+                <Route path="best" element={<CollectionsPage />} />
+              </Route>
+              <Route path="cart" element={<Cart />} />
+              <Route path="checkout" element={<CheckoutPage />} />
+              <Route path="payment" element={<PaymentPage />} />
+              <Route path="profile" element={<UserProfilePage />} />
             </Route>
-            <Route path="collections" element={<CollectionsPage />}>
-              <Route path="new" element={<CollectionsPage />} />
-              <Route path="rare" element={<CollectionsPage />} />
-              <Route path="best" element={<CollectionsPage />} />
+            <Route path="/admin" element={<AdminHeader />}>
+              <Route path="home" element={<AdminHomePage />} />
+              <Route path="product" element={<AdminProductPage />} />
+              <Route path="stock" element={<AdminStockPage />} />
+              <Route path="order" element={<AdminOrderPage />} />
+              <Route path="user" element={<AdminUserPage />} />
             </Route>
-            <Route path="cart" element={<Cart />} />
-            <Route path="checkout" element={<CheckoutPage />} />
-            <Route path="payment" element={<PaymentPage />} />
-            <Route path="profile" element={<UserProfilePage />} />
-          </Route>
-          <Route path="/admin" element={<AdminHeader />}>
-            <Route path="home" element={<AdminHomePage />} />
-            <Route path="product" element={<AdminProductPage />} />
-            <Route path="stock" element={<AdminStockPage />} />
-            <Route path="order" element={<AdminOrderPage />} />
-            <Route path="user" element={<AdminUserPage />} />
-          </Route>
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   );
 }
--- src/pages/HomePage/Carousel.js
+++ src/pages/HomePage/Carousel.js
@@ -9,68 +9,48 @@ import "swiper/css/navigation";
 // import required modules
 import { Keyboard } from "swiper";
 import slidesrc from "assets/images/Slide.png";
+import slidesrc2 from "assets/images/Slide2.jpg";
 
 const Carousel = () => {
   return (
     <>
       <Swiper
+        autoplay={true}
         slidesPerView={1}
         spaceBetween={30}
         keyboard={{
           enabled: true,
         }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
         modules={[Keyboard]}
         className="mySwiper"
         effect="cards"
       >
         <SwiperSlide>
-          <div className="swiper-zoom-container">
-            <img src={slidesrc} alt="slid" />
-          </div>
+          <img src={slidesrc} alt="slid" width={2000} height={500} />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="swiper-zoom-container">
-            <img src={slidesrc} alt="slid" />
-          </div>
+          <img src={slidesrc2} alt="slid" width={2000} height={500} />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="swiper-zoom-container">
-            <img src={slidesrc} alt="slid" />
-          </div>
+          <img src={slidesrc} alt="slid" width={2000} height={500} />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="swiper-zoom-container">
-            <img src={slidesrc} alt="slid" />
-          </div>
+          <img src={slidesrc2} alt="slid" width={2000} height={500} />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="swiper-zoom-container">
-            <img src={slidesrc} alt="slid" />
-          </div>
+          <img src={slidesrc} alt="slid" width={2000} height={500} />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="swiper-zoom-container">
-            <img src={slidesrc} alt="slid" />
-          </div>
+          <img src={slidesrc2} alt="slid" width={2000} height={500} />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="swiper-zoom-container">
-            <img src={slidesrc} alt="slid" />
-          </div>
+          <img src={slidesrc} alt="slid" width={2000} height={500} />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="swiper-zoom-container">
-            <img src={slidesrc} alt="slid" />
-          </div>
+          <img src={slidesrc2} alt="slid" width={2000} height={500} />
         </SwiperSlide>
         <SwiperSlide>
-          <div className="swiper-zoom-container">
-            <img src={slidesrc} alt="slid" />
-          </div>
+          <img src={slidesrc} alt="slid" width={2000} height={500} />
         </SwiperSlide>
       </Swiper>
     </>
