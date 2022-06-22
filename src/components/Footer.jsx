92654933ebc36372091c120f7f3908ffe8c65296
CapBarbosssa <97461923+CapBarbosssa@users.noreply.github.com>
2022-06-21 21:41:43 -0600

    create Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,41 @@ import React from "react";
 
 function Footer() {
   return (
-    <Box>
+    <Box bgcolor="#F5F5F5" color="black">
       <Container maxWidth="lg">
-        <Grid container spacing={5}>
+        <Grid container padding={10}>
           <Grid item xs={12} sm={4}>
-            <Box borderBottom={1}>Help </Box>
             <Box>
-              <Link href="/" color="inherit">
+              <Link href="/" color="inherit" fontWeight="bold" fontSize="25px">
                 Contact Us
               </Link>
             </Box>
             <Box>
-              <Link href="/" color="inherit">
-                Follow Us Us
+              <p> 774 Gordon Baker Rd</p>
+              <p>North York, ON M2H 3B4</p>
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} sm={4}>
+            <Box>
+              <Link href="/" color="inherit" fontWeight="bold" fontSize="25px">
+                Store Hours
               </Link>
+            </Box>
+            <Box>
+              <p>Mon-Friday : 1pm-6pm</p>
+              <p>Sat : 2pm-6pm</p>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Box>
+              <Link href="/" color="inherit" fontWeight="bold" fontSize="25px">
+                Follow Us
+              </Link>
+            </Box>
+            <Box>
+              <p>Instagram</p>
+              <p>WeChat</p>
             </Box>
           </Grid>
         </Grid>
--- src/pages/CheckoutPage/CheckoutPage.js
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import CheckoutCart from "./CheckoutCart";
+import Footer from "components/Footer";
 
 const CheckoutPage = () => {
   return (
     <div>
       <CheckoutCart />
+      <Footer />
     </div>
   );
 };
