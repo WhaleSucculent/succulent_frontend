88928f10286d8f0e7a0e300fbe74991cd8c26bfa
samohib <98845731+samohib@users.noreply.github.com>
2022-07-02 15:58:25 -0600

    message

--- src/pages/ProductDetailPage/ProductDetailPage.js
+++ src/pages/ProductDetailPage/ProductDetailPage.js
@@ -1,9 +1,115 @@
-import React from 'react'
+import {
+  Dialog,
+  DialogTitle,
+  Slide,
+  Box,
+  IconButton,
+  DialogContent,
+  Typography,
+  Button,
+  Stack,
+} from "@mui/material";
+import { useRef } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import { Colors } from "./Themes";
+import styled from "@emotion/styled";
+import { ProductAddToCart, Product, ProductImage } from "./Product";
+//import { BannerShopButton } from "../../styles/banner";
+//import IncDec from "../ui/incdec";
+import {itemData} from '../CheckoutPage/fakedata';
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useTheme } from "@mui/material/styles";
+import { useMediaQuery } from "@mui/material";
 
-const ProductDetailPage = () => {
-  return (
-    <div>ProductDetailPage</div>
-  )
+function SlideTransition(props) {
+  return <Slide direction="down" {...props} />;
 }
 
-export default ProductDetailPage
+const ProductDetailWrapper = styled(Box)(({ theme }) => ({
+  display: "flex",
+  padding: theme.spacing(4),
+}));
+
+const ProductDetailInfoWrapper = styled(Box)(() => ({
+  display: "flex",
+  flexDirection: "column",
+  maxWidth: 500,
+  lineHeight: 1.5,
+}));
+
+export default function ProductDetailPage({open, onClose}) {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down("md"));
+  return (
+    <Dialog
+      TransitionComponent={SlideTransition}
+      variant="permanant"
+      open={open}
+      fullScreen
+    >
+      <DialogTitle
+        sx={{
+          //background: Colors.secondary,
+        }}
+      >
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent={"space-between"}
+        >
+          Product title
+          <IconButton onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+      </DialogTitle>
+      <DialogContent>
+        <ProductDetailWrapper display={"flex"} flexDirection={matches ? "column" : "row"}>
+          <Product sx={{ mr: 4 }}>
+            <ProductImage src={itemData[0].img} />
+          </Product>
+          <ProductDetailInfoWrapper>
+            <Typography variant="subtitle">SKU: 123</Typography>
+            <Typography variant="subtitle">Availability: 5 in stock</Typography>
+            <Typography sx={{ lineHeight: 2 }} variant="h4">
+              hELLO
+            </Typography>
+            <Typography variant="body">
+             hELLO
+            </Typography>
+            <Box
+              sx={{ mt: 4 }}
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+            >
+          
+              <Button variant="contained">Add to Cart</Button>
+            </Box>
+            <Box
+              display="flex"
+              alignItems="center"
+              //sx={{ mt: 4, color: Colors.light }}
+            >
+              <FavoriteIcon sx={{ mr: 2 }} />
+              Add to wishlist
+            </Box>
+            <Box
+              sx={{
+                mt: 4,
+                //color: #e6f2ff,
+              }}
+            >
+              <FacebookIcon />
+              <TwitterIcon sx={{ pl: 2 }} />
+              <InstagramIcon sx={{ pl: 2 }} />
+            </Box>
+          </ProductDetailInfoWrapper>
+        </ProductDetailWrapper>
+      </DialogContent>
+    </Dialog>
+  );
+}
