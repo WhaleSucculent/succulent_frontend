ce8b615bf8f435490232e61bad58a22a5c308ea4
flowersmiling <lcl8885@gmail.com>
2022-07-21 13:42:09 -0600

    update payment

--- src/pages/PaymentPage/purchase.js
+++ src/pages/PaymentPage/purchase.js
@@ -11,7 +11,7 @@ export const Purchase = ({ price, tag, productIds, searchQuery, children }) => {
 			  'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({
-			  totalAmount: price,
+			  totalAmount: parseInt(price),
 			  cancelRoute: window.location.pathname,
 			  productIds: productIds,
 			  searchQuery: searchQuery,
