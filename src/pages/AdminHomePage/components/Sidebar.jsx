0ed9f0e6ac25c3da4efefd14f28f9d5bd676815e
Jiasheng Lu <1473875731@qq.com>
2022-08-02 15:31:29 -0600

    fix: router in admin

--- src/pages/AdminHomePage/components/Sidebar.jsx
+++ src/pages/AdminHomePage/components/Sidebar.jsx
@@ -6,27 +6,20 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
-import InventoryIcon from '@mui/icons-material/Inventory';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import StorefrontIcon from '@mui/icons-material/Storefront';
-import Users from '../pages/Users';
-import Inventory from '../pages/Inventory';
-import Order from '../pages/Order';
-import { Link } from '@mui/material';
-import UserEdit from '../pages/UserEdit';
-import Home from '../pages/Home';
+import Link from 'components/Link';
+import { Outlet } from 'react-router-dom';
 const drawerWidth = 240;
 
 function Sidebar(props) {
@@ -42,53 +35,62 @@ function Sidebar(props) {
       <Toolbar />
       <Divider />
       <List>
-        
-          <ListItem >
+        <ListItem >
+          <Link to={'/admin/home'} color="inherit" underline='hover'>
             <ListItemButton>
               <ListItemIcon>
-               <HomeIcon /> 
+                <HomeIcon />
               </ListItemIcon>
-              <ListItemText  primary="Home"/>
+              <ListItemText primary="Home" />
             </ListItemButton>
-          </ListItem>
-          <ListItem >
+          </Link>
+        </ListItem>
+        <Divider />
+        <ListItem >
+          <Link to={'/admin/user'} color="inherit" underline='hover'>
             <ListItemButton>
               <ListItemIcon>
-               <PersonIcon /> 
+                <PersonIcon />
               </ListItemIcon>
-              <ListItemText  primary="User"><Link href="/admin/user" underline="none">
-  {'underline="none"'}
-</Link></ListItemText>
+              <ListItemText primary="User"><Link href="/admin/user" underline="none">
+                {'underline="none"'}
+              </Link></ListItemText>
             </ListItemButton>
-          </ListItem>
-          <ListItem >
+          </Link>
+        </ListItem>
+        <Divider />
+        <ListItem >
+          <Link to={'/admin/product'} color="inherit" underline='hover'>
             <ListItemButton>
               <ListItemIcon>
-               <StorefrontIcon /> 
+                <StorefrontIcon />
               </ListItemIcon>
-              <ListItemText  primary="Inventory"/>
+              <ListItemText primary="Inventory" />
             </ListItemButton>
-          </ListItem>
-          <ListItem >
+          </Link>
+        </ListItem>
+        <Divider />
+        <ListItem >
+          <Link to={'/admin/order'} color="inherit" underline='hover'>
             <ListItemButton>
               <ListItemIcon>
-               <ShoppingBasketIcon /> 
+                <ShoppingBasketIcon />
               </ListItemIcon>
-              <ListItemText  primary="Order"/>
+              <ListItemText primary="Order" />
             </ListItemButton>
-          </ListItem>
-          <ListItem >
-            <ListItemButton>
-              <ListItemIcon>
-               <HomeIcon /> 
-              </ListItemIcon>
-              <ListItemText  primary="Single-line item"/>
-            </ListItemButton>
-          </ListItem>
-    
+          </Link>
+        </ListItem>
+        <Divider />
+        <ListItem >
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Single-line item" />
+          </ListItemButton>
+        </ListItem>
+        <Divider />
       </List>
-      <Divider />
-
     </div>
   );
 
@@ -96,85 +98,71 @@ function Sidebar(props) {
 
   return (
     <>
-    
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+          }}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Admin Dashbaord
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        >
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Admin Dashbaord
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
+            {drawer}
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: 'none', sm: 'block' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
         >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
+          <Toolbar>
+              <Outlet/>
+          </Toolbar>
+        </Box>
       </Box>
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
-      >
-        <Toolbar />
-        <Typography paragraph>
-          <Home/>
-        </Typography>
-        <Typography paragraph>
-          <Users/>
-        </Typography>
-        <Typography paragraph>
-         <Inventory/>
-        </Typography>
-        <Typography paragraph>
-         <Order/>
-        </Typography>
-        <Typography paragraph>
-         <UserEdit/>
-        </Typography>
-      </Box>
-    </Box>
     </>
   );
 }
