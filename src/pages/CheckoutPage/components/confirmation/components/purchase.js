083ba1188eb3a4d0c68762eb7e9b7296962d5039
Jiasheng Lu <1473875731@qq.com>
2022-09-10 14:51:52 -0600

    update backend url

--- src/pages/CheckoutPage/components/confirmation/components/purchase.js
+++ src/pages/CheckoutPage/components/confirmation/components/purchase.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 
 export const Purchase = ({ price, tag, productIds, searchQuery, children }) => {
 	const handleBuy = () => {
-		fetch('http://35.183.181.172/api/checkout/payment-api', {
+		fetch('https://succulentwhales.azurewebsites.net/api/checkout/payment-api', {
 			method: 'POST',
 			headers: {
 			  'Content-Type': 'application/json',
